0910c7bc9671eef377e15c5bf0f445d98d8fefb2
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-06-15 21:45:26 +0530

    Added backend logic for hitting Url

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,37 @@
 import express from "express";
+import dotenv from "dotenv";
+import axios from 'axios';
+const app = express();
+dotenv.config();
+const port = process.env.PORT;
+
+
+const apiKey = process.env.WEATHER_API_KEY;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/weather',async (req, res) => {
+    try {
+        const city = req.query.city || 'delhi';
+        const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+        const response = await axios.get(apiUrl);
+        const weatherData = response.data.main.temp;
+        console.log(weatherData);
+           res.send(weatherData);
+       
+
+    }
+    catch (error) {
+        console.error('Error fetching weather data:', error);
+        res.status(500).send('Error fetching weather data');
+    }
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
