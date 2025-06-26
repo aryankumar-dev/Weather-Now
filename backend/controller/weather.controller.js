5dda951bf2bf09d08ee4de0d261bcd7563f3d371
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-06-26 18:50:22 +0530

    Redeploy with changes

--- backend/controller/weather.controller.js
+++ backend/controller/weather.controller.js
@@ -1,20 +1,20 @@
-import {Weather} from "../models/weather.model.js";
+import { Weather } from "../models/weather.model.js";
 
-const addweather = async(req,res) =>{
+const addweather = async (req, res) => {
 
-    const {city,temprature} = req.body;
+  const { city, temprature } = req.body;
 
 
   const createdata = await Weather.create({
     city,
-    
+
     temprature
   });
 
 
-  console.log("data added ")
+  console.log("weather added in database ")
   return res.status(200).send(createdata);
- 
+
 
 }
 
